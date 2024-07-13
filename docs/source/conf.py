124f7fdbb6687797b44e9a3b584eea4a55901b10
Danweel <danweel.the.kelpie@gmail.com>
2024-07-13 18:51:05 -0400

    Setting up Furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,4 +36,11 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+html_static_path = ['..\build\html\source\_static']
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": "red",
+        "color-brand-content": "#CC3333",
+        "color-admonition-background": "orange",
+    },
+}
